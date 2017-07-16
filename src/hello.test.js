2490983722a1081f428e4ba1cb08db0532d0d4c3
Salvatore Previti <sp@wopr.home>
2017-07-16 16:12:02 +0100

    initial

--- src/hello.test.js
+++
@@ -1,9 +0,0 @@
-const idx = require('./index');
-
-console.log(idx);
-
-describe('hello', () => {
-  it('should print hello world', () => {
-    expect(1).toBe(1);
-  });
-});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
 console.log('hello world.');
-
-module.exports = 'hello world';
