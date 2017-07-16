b2f7c741449df7f24a4f5eb1e91c171989a50fe7
Salvatore Previti <sp@wopr.home>
2017-07-16 16:10:46 +0100

    initial

---
+++ index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node --use_strict
+
+/* eslint-disable */
+
+"use strict";
+
+require('./src/index.js');
---
+++ src/hello.test.js
@@ -0,0 +1,9 @@
+const idx = require('./index');
+
+console.log(idx);
+
+describe('hello', () => {
+  it('should print hello world', () => {
+    expect(1).toBe(1);
+  });
+});
